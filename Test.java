9ea3446eb2ac15b52cac8355587da34fc451fcc2
Robina-Barakzai <33723017+Robina-Barakzai@users.noreply.github.com>
2017-11-21 20:57:40 +0430

    Update to Test.java
    I have completed my job

--- Test.java
+++ Test.java
@@ -1,4 +1,22 @@
-class Person{}
+class Person{
+ Person() {
+    autoId++;
+    id = autoId;
+  }
+
+  void set(String name, String address, int age) {
+    this.name = name;
+    this.address = address;
+    this.age = age;
+  }
+
+  void get() {
+    System.out.println("ID: " + id);
+    System.out.println("Name: " + name);
+    System.out.println("age: " + age);
+    System.out.println("address: " + address);
+  }
+}
 
 class Condidate extends Person{}
 
