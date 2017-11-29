148ea3a9b718ae2b2bad50e5915bb9eb637c2a9e
nazarkdh <nazarkdh@gmail.com>
2017-11-29 12:04:46 +0430

    completed the code

--- Test.java
+++ Test.java
@@ -1,11 +1,97 @@
-class Person{}
+class Person {
+  private int id;
+  private static int autoId;
+  protected String name;
+  private String address;
+  protected int age;
+  protected static int[] vote=new int[5];
 
-class Condidate extends Person{}
+  Person() {
+    autoId++;
+    id = autoId;
+  }
 
-class Voters extends Person{}
+  void set(String name, String address, int age) {
+    this.name = name;
+    this.address = address;
+    this.age = age;
+  }
 
-class Test{
-	public static void main(String[] args) {
-		
-	}
+  void get() {
+    System.out.println("ID: " + id);
+    System.out.println("Name: " + name);
+    System.out.println("age: " + age);
+    System.out.println("address: " + address);
+  }
 }
+
+class Voter extends Person {
+  private boolean voteState;
+
+  void vote(int rollNo) {
+    if (voteState == false) {
+      if (age >= 18) {
+        
+            vote[rollNo-1]++;
+            voteState=true;
+            System.out.println("your vote is submitted successfully");
+           
+      } else System.out.println("you are not eligible to vote");
+    } else System.out.println("You cant vote again");
+  }
+}
+
+class Condidate extends Person {
+  private int rollNo;
+  private static int autoNo;
+
+  Condidate() {
+    autoNo++;
+    rollNo = autoNo;
+  }
+
+  void get() {
+    super.get();
+    System.out.println("Roll NO: " + rollNo);
+  }
+
+  void getVote() {
+        System.out.println("Your total votes are: " +vote[rollNo-1]);
+  }
+  void getPercentage(){
+    int total=vote[0]+vote[1]+vote[2]+vote[3]+vote[4];
+    float percent=vote[rollNo-1]*100/total;
+    System.out.println("Dear "+name+" You got "+percent+"% of total votes");
+  }
+}
+
+public class Test{
+  public static void main(String[] args) {
+
+    Voter p1 = new Voter();
+    p1.set("Ahmad", "Kandahar", 18);
+    Condidate Abdullah = new Condidate();
+    Abdullah.set("Doctor Abdullah", "Panjsheer", 45);
+    Voter Nazar = new Voter();
+    Nazar.set("Nazar Mohammad", "Kandahar", 26);
+    Condidate Ashraf_Ghani = new Condidate();
+    Ashraf_Ghani.set("Ashraf Ghani", "Kabul", 50);
+    Voter khan=new Voter();
+    khan.set("Khan Mohammad","Logar",30);
+    
+
+    //Ashraf_Ghani.get();
+    //Nazar.get();
+   // Abdullah.get();
+   Nazar.vote(2);
+   //Nazar.vote(1);
+  // khan.get();
+   khan.vote(1);
+   p1.vote(2);
+   Abdullah.getVote();
+   Ashraf_Ghani.getVote();
+   Abdullah.getPercentage();
+   Ashraf_Ghani.getPercentage();
+  }
+}
+
